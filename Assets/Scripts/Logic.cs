21e52127826f4d9b170c474322c0296c949b1369
trance963 <94801903+trance963@users.noreply.github.com>
2022-06-28 21:54:27 +0400

    0.02
    Add Victims, base logic

---
+++ Assets/Scripts/Logic.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logic : MonoBehaviour
+{
+    public enum State // игровые состояния
+    {
+        Playing,
+        Won,
+        Loss,
+    }
+
+    public State CurrentState { get; private set; }
+
+    public void TooMuch() // метод смерти
+    {
+        if (CurrentState != State.Playing) return;
+
+        CurrentState = State.Loss;
+        Debug.Log("GameOver!");
+    }
+
+    public void HundredPoints() // метод финиша
+    {
+        if (CurrentState != State.Playing) return;
+
+        CurrentState = State.Won;
+        Debug.Log("YouWon!");
+    }
+}
